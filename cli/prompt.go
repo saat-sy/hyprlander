3493767f640e99b925e6f4ec474174eff0bbd5fc
Saatwik Yajaman <saatwik.sy@gmail.com>
2025-10-05 13:02:50 -0700

    feat: added fetch hypr tree
    BREAKING CHANGE: .hypr is not predefined. would be better to add that in the init

--- cli/prompt.go
+++ cli/prompt.go
@@ -1,6 +1,9 @@
 package cli
 
 import (
+	"fmt"
+
+	"github.com/saat-sy/hyprlander/pkg/config"
 	"github.com/saat-sy/hyprlander/pkg/core/agent"
 	"github.com/spf13/cobra"
 )
@@ -10,12 +13,23 @@ func PromptCommand() *cobra.Command {
 		Use:   "prompt",
 		Short: "Execute prompt-based hyprland configuration changes",
 		Long:  "Use natural language prompts to modify hyprland configuration files",
-		Run: func(cmd *cobra.Command, args []string) {
-			agent := agent.NewAgent()
+		RunE: func(cmd *cobra.Command, args []string) error {
+			hyprPath, err := config.GetHyprInstallationPath()
+			if err != nil {
+				return fmt.Errorf("error getting Hypr installation path :%v. Please install hyprland before you can use hyprlander", err)
+			}
+
+			hyprlandPathStructure, err := config.GetTreeFromDir(hyprPath)
+			if err != nil {
+				return fmt.Errorf("error reading Hypr installation directory: %v", err)
+			}
+
+			agent := agent.NewAgent(hyprlandPathStructure)
 			if len(args) > 0 {
 				prompt := args[0]
 				agent.InvokeAgent(prompt)
 			}
+			return nil
 		},
 	}
 
